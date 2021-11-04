ab3c474377011d6d952d4f3b37eb0da7a631a914
tbb33 <91215510+tbb33@users.noreply.github.com>
2021-11-04 13:31:49 -0500

    Delete_command function, use bind
    User will select row from listbox then select delete button
    1. Use Bind from Tkinter: binds get_selected_row function to listbox widget, returns list object with selection (tuple)
    2. Define get_selected_row function: get index as single nbr from selected row in listbox, then gets row with that index in global var selected_tuple
    3. Define delete_command function: deletes row in backend using selected_tuple
    4. Edit get_selected_row function: when user selects row in listbox, display that row in the 4 entry widgets

--- frontend.py
+++ frontend.py
@@ -15,6 +15,27 @@ import tkinter
 from tkinter import *
 import backend
 
+def get_selected_row(event):
+    global selected_tuple
+    #grabs 1st item from tuple-index from listbox as single nbr
+    index = list.curselection()[0]
+    #print(index) #test: prints to terminal
+    selected_tuple = list.get(index) #gets tuple from listbox with specified index
+    #print(selected_tuple[0]) #test
+    #return(selected_tuple) #test - not needed when global var
+
+    #when user selects entry from listbox,
+    #insert those 4 values into entry widgets
+    e1.delete(0,END)
+    e1.insert(END,selected_tuple[1]) #insert title to title entry widget
+    e3.delete(0,END)
+    e3.insert(END,selected_tuple[2]) #author
+    e2.delete(0,END)
+    e2.insert(END,selected_tuple[3]) #year
+    e4.delete(0,END)
+    e4.insert(END,selected_tuple[4]) #isbn
+
+
 #in backend we have list of tuples
 #view function inserts each tuple as new row inside Listbox
 def view_command():
@@ -36,6 +57,9 @@ def add_command():
     list.delete(0,END)
     #show new entry (as tuple) in listbox
     list.insert(END, (title_value.get(),author_value.get(),year_value.get(),isbn_value.get()))
+
+def delete_command():
+    backend.delete(selected_tuple[0]) #passes index only
 
 window=Tk()
 window.title("Bookstore Inventory")
@@ -76,7 +100,7 @@ b3=Button(window, text="Add entry",width=14, command=add_command)
 b3.grid(row=4,column=3)
 b4=Button(window, text="Update",width=14)
 b4.grid(row=5,column=3)
-b5=Button(window, text="Delete",width=14)
+b5=Button(window, text="Delete",width=14, command=delete_command)
 b5.grid(row=6,column=3)
 b6=Button(window, text="Close",width=14)
 b6.grid(row=7,column=3)
@@ -91,5 +115,8 @@ s.grid(row=2,column=2, rowspan=6)
 #apply config method to listbox and scrollbar obj
 list.configure(yscrollcommand=s.set)
 s.configure(command=list.yview) #when scroll the vertial view chages
+
+#bind function (get_selected_row) to widget event (ListboxSelect)
+list.bind('<<ListboxSelect>>', get_selected_row)
 
 window.mainloop()
